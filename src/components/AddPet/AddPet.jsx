f4bf2d1f861358ac31857bee5fdd89835cf8a9cd
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-09-04 12:19:48 -0500

    image upload with cloudinary

--- src/components/AddPet/AddPet.jsx
+++ src/components/AddPet/AddPet.jsx
@@ -1,8 +1,24 @@
+import axios from "axios"
+import { useState } from "react"
+
 function AddPet() {
+
+    const [image, setImage] = useState("")
+
+    const uploadImage = () => {
+        const formData = new FormData();
+        formData.append("file", image)
+        formData.append("upload_preset", "PetEats")
+        axios.post("https://api.cloudinary.com/v1_1/dzyea2237/image/upload", formData).then((response) => {
+            console.log(response)
+        })
+    }
+
     return (
         <>
             <h3>Add A New Pet</h3>
-            
+            <input type="file" onChange={(e) => {setImage(e.target.files[0])}}/>
+            <button onClick={uploadImage()}>Upload</button>
         </>
     )
 }
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import {Image} from 'cloudinary-react'
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
@@ -13,6 +14,7 @@ function UserPage() {
       <p>Your ID is: {user.id}</p>
       <LogOutButton className="btn" />
       <button onClick={()=> history.push('/addpet')}>Add Pet</button>
+      <Image cloudName="dzyea2237" publicId="https://res.cloudinary.com/dzyea2237/image/upload/v1662310851/a6nubuj2lz9jzg7d5nzi.jpg"/* save image to database and pass the saved url in here *//>
     </div>
   );
 }
