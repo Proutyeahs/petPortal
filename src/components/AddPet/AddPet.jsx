43559587718e5895036b58e3830180389fe8fb26
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-09-07 12:43:35 -0500

    everythings working

--- src/components/AddPet/AddPet.jsx
+++ src/components/AddPet/AddPet.jsx
@@ -1,4 +1,4 @@
-import axios from "axios"
+
 import React from 'react';
 import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
--- src/components/PetDetails/PetDetails.jsx
+++ src/components/PetDetails/PetDetails.jsx
@@ -6,17 +6,12 @@ import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import './PetDetails.css'
-import Box from '@material-ui/core/Box';
-import Collapse from '@material-ui/core/Collapse';
-import IconButton from '@material-ui/core/IconButton';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
 
 function PetDetails() {
 
